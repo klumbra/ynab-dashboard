46221c3f0818b63b3c1cee0199e58aa71b505ce5
Kyle Lumbra <klumbra@gmail.com>
2018-12-22 14:32:24 -0500

    Get ynab cat/month data

---
+++ main.py
@@ -0,0 +1,37 @@
+def get_ynab_month(month):
+	import ynab
+
+	configuration = ynab.Configuration()
+	configuration.api_key['Authorization'] = get_config_val('ynab_api_key')
+	configuration.api_key_prefix['Authorization'] = 'Bearer'
+	api_instance = ynab.MonthsApi(ynab.ApiClient(configuration))
+	budget_id = get_config_val('ynab_budget_id')
+	api_response = api_instance.get_budget_month(budget_id, month)
+	return api_response
+
+def get_config_val(key):
+	import configparser
+
+	config = configparser.ConfigParser()
+	config.read('config.ini')
+	return config['main'][key]
+
+def extract_ynab_cat_attrs(month, api_response):
+	cat_attrs = []
+	for category in api_response.data.month.categories:
+		name = category.name
+		budgeted = category.budgeted / 1000.0
+		cat_attrs.append((month, name, budgeted))
+	return cat_attrs
+
+def test_ynab():
+	import pprint
+	month = '2018-12-01'
+	api_response = get_ynab_month(month)
+	cat_attrs = extract_ynab_cat_attrs(month, api_response)
+	pprint.pprint(cat_attrs)
+
+def main():
+	test_ynab()
+
+main()
